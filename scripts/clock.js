633b2af54b244cb0cabc5dbda6c92ad7ae1544fe
Bryan-OLima <bryanosoriolima@gmail.com>
2022-08-23 13:06:58 -0300

    Styles and HTML added

---
+++ scripts/clock.js
@@ -0,0 +1,1 @@
+window.clock = new Clock();
---
+++ scripts/controllers/clockController.js
@@ -0,0 +1,18 @@
+class Clock{
+    constructor(){
+        this.initialize();
+    }
+
+    initialize(){
+        this.time();
+        this.date();
+    }
+
+    time(){
+        
+    }
+
+    date(){
+        
+    }
+}
