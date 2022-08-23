15be65556cfe5f5bd98b0f619eeff36cfe3cdd46
Bryan-OLima <bryanosoriolima@gmail.com>
2022-08-23 13:36:38 -0300

    logics done

--- scripts/controllers/clockController.js
+++ scripts/controllers/clockController.js
@@ -1,18 +1,40 @@
 class Clock{
-    constructor(){
+    constructor(){  
+        this._displayTimeEl = document.querySelector('.dt'); 
+        this._currentDate;
+        let _locale = 'pt-BR';
         this.initialize();
     }
 
     initialize(){
-        this.time();
-        this.date();
+        this.setTime();
+        setInterval( ()=> {
+            this.setTime()
+        }, 1000);
     }
 
-    time(){
-        
+    setTime(){
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+        console.log(this._displayTimeEl);
     }
 
-    date(){
-        
+/** GETTERS AND SETTERS START**/
+
+    get displayTime(){
+        return this._displayTimeEl.innerHTML;
     }
+
+    set displayTime(value){
+        return this._displayTimeEl.innerHTML = value;
+    }
+
+    get currentDate(){
+        return new Date();
+    }
+
+    set currentDate(value){
+        return this._currentDate = value;
+    }
+
+/** GETTERS AND SETTERS END**/
 }
